f03cb04b470e2c43506dd1ee222e508c97a6dc49
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2021-06-02 10:20:16 -0600

    First commit

---
+++ NCAPAS_NETCORE/CAPA_DATOS/SqlConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace CAPA_DATOS
+{
+    public class SqlConnection
+    {
+    }
+}
---
+++ NCAPAS_NETCORE/CAPA_NEGOCIO/ClassStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAPA_NEGOCIO
+{
+    public class ClassStore
+    {
+        public int IdStore { get; set; }
+        public string Nombre { get; set; }
+        public bool CreateStore()
+        {            
+            try
+            {
+                //implementar la funcion Create
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
